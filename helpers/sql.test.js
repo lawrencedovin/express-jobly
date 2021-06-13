5b08feb465eec33e7f23cd879ff8a01b964adaff
Lawrence Dovin <lawrencedovin@gmail.com>
2021-06-13 15:48:24 -0400

    Added first unit test for sqlForPartialUpdate

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,2 +1,14 @@
-const { sqlForPartialUpdate } = require('./sql');
-const jwt = require("jsonwebtoken");
+const { sqlForPartialUpdate } = require("./sql");
+
+describe("sqlForPartialUpdate", () => {
+  test("Does a single column update", () => {
+    const result = sqlForPartialUpdate(
+      { firstName: "lawrence" },
+      { firstName: "first_name" }
+    );
+    expect(result).toEqual({
+      setCols: '"first_name"=$1',
+      values: ["lawrence"]
+    });
+  });
+});
