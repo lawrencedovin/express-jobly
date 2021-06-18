22700177eaa37c0eee6d723ac074062265153c27
Lawrence Dovin <lawrencedovin@gmail.com>
2021-06-18 16:01:39 -0400

    Updated company test model to add jobs for get by company handle

--- models/company.test.js
+++ models/company.test.js
@@ -8,6 +8,7 @@ const {
   commonBeforeEach,
   commonAfterEach,
   commonAfterAll,
+  testJobIds,
 } = require("./_testCommon");
 
 beforeAll(commonBeforeAll);
@@ -179,6 +180,11 @@ describe("get", function () {
       description: "Desc1",
       numEmployees: 1,
       logoUrl: "http://c1.img",
+      jobs: [
+        { id: testJobIds[0], title: "Job1", salary: 100, equity: "0.1" },
+        { id: testJobIds[1], title: "Job2", salary: 200, equity: "0.2" },
+        { id: testJobIds[2], title: "Job3", salary: 300, equity: "0" },
+      ],
     });
   });
 
@@ -191,6 +197,7 @@ describe("get", function () {
     }
   });
 });
+
 
 /************************************** update */
 
